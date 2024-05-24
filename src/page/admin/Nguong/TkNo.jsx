655b49ebad7c6d471debef14654918388c6d2916
tuana8tmtt <49206137+tuana8tmtt@users.noreply.github.com>
2024-05-24 11:10:39 +0700

    update nguong live, die, tk no, khong nguong

--- src/page/admin/Nguong/TkNo.jsx
+++ src/page/admin/Nguong/TkNo.jsx
@@ -1,37 +1,17 @@
 import { useEffect, useState } from 'react';
-import { Table, theme } from 'antd';
+import { Table, message, theme } from 'antd';
 import { Content } from 'antd/es/layout/layout';
-import { getlistCookie } from '../../../api/allCookie';
 import Title from 'antd/es/typography/Title';
 import Button from 'antd-button-color';
+import moment from 'moment';
+import { Input } from "antd";
+import { Space, Typography } from 'antd';
+import { getlistUserAds } from '../../../api/userAds';
 
-const columns = [
-    {
-        key: '_id',
-        sorter: (a, b) => a._id - b._id,
-        render: (record) => (
-            <>
-                <div dangerouslySetInnerHTML={{__html: record[1]}}>
-                    
-                </div>
-            </>
-        ),
-        width: '20%',
-    },
-    {
-        title: 'Info ADS',
-        key: '_id',
-        sorter: true,
-        render: (record) => (
-            <>
-                <div dangerouslySetInnerHTML={{__html: record[2]}}>
-                    
-                </div>
-            </>
-        ),
-    },
 
-];
+
+
+
 const TkNo = () => {
     const [data, setData] = useState();
     const [loading, setLoading] = useState(false);
@@ -41,14 +21,93 @@ const TkNo = () => {
             pageSize: 10,
         },
     });
+    const [messageApi, contextHolder] = message.useMessage();
+    const { Text, Link } = Typography;
     const {
         token: { colorBgContainer, borderRadiusLG },
     } = theme.useToken();
+    const columns = [
+        {
+            key: '_id',
+            sorter: (a, b) => a._id - b._id,
+            render: (record) => {
+                function handleCopy(data) {
+                    navigator.clipboard.writeText(data)
+                    if (data) {
+                        messageApi.open({
+                            type: 'success',
+                            content: 'Bạn đã sao chép thành công',
+                        });
+                    }
+                    else {
+                        messageApi.open({
+                            type: 'error',
+                            content: 'Tài khoản không có thông tin',
+                        });
+                    }
+                }
+                const apitime = record.updatedAt;
+                const fixedtime = moment.utc(apitime).local();
+                console.log(record)
+                return (
+                    <>
+                        <div className="cl_1">
+                            <span className="udDate">Ngày Cập Nhật: <br /> {fixedtime.format("DD-MM-YYYY HH:mm:ss A")}</span>
+                            <div className="uid_a">UID: <span id="uid" onClick={() => handleCopy(record.cookie_data[0].uid)}>{record.cookie_data[0].uid}</span>
+                            </div>
+                            <div className="btn_box">
+                                <Button type="primary" className="cpck_btn" onClick={() => handleCopy(record.cookie_data[0].cookie)}>Copy Cookie</Button>
+                                <Button type="success" className="cpp_btn" onClick={() => handleCopy(record.cookie_data[0].password)}>Copy Pass</Button>
+                                <Button type="info" className="cpif_btn" onClick={() => handleCopy(record.cookie_data[0].info)}>Copy Info</Button>
+                                <Button type="lightdark" className="cpif_btn" onClick={() => handleCopy(record.cookie_data[0].access_token)}>Copy Token</Button>
+                            </div>
+                        </div>
+                    </>
+                )
+            },
+            width: '30%',
+
+        },
+        {
+            title: 'Info ADS',
+            key: '_id',
+            sorter: true,
+            render: (record) => {
+                function check_status(data) {
+                    var a
+                    switch (data) {
+                        case 1:
+                            return a = <Text type="success" className="active">Hoạt động</Text>;
+                        case 2:
+                            return a = <Text type="danger" className="inactive">Vô hiệu hóa</Text>;
+                        case 3:
+                            return a = <Text type="warning" className="no">Nợ</Text>;
+                    }
+                }
+                return (
+                    <>
+                        <div className="cl_2">
+                            <div className="status">
+                                <span>- {check_status(record.account_status)}</span>
+                            </div>
+                            <div className="Friend">- Bạn bè: {record.cookie_data[0].friend}</div>
+                            <div className="limit">- Giới hạn: {record.limit} ({record.user_roles})</div>
+                            <div className="nguong">- Ngưỡng: {record.nguong}</div>
+                            <div className="total_price">- Tổng tiêu: {record.total_pay}</div>
+                            <div className="balance">- Số dư: {record.balance}</div>
+                            <div className="createdAt">- Ngày tạo: {moment(record.createdAt).format("DD-MM-YYYY HH:mm:ss A")}</div>
+                        </div>
+                    </>)
+            }
+        },
+
+    ];
     const fetchData = async () => {
         setLoading(true);
-        await getlistCookie()
+        await getlistUserAds()
             .then((res) => {
-                setData(res.data);
+                const data = res.data.filter(item => item.account_status === 3)
+                setData(data);
                 setLoading(false)
             })
 
@@ -70,6 +129,7 @@ const TkNo = () => {
         }
     };
     return (
+
         <Content
             style={{
                 margin: '24px 16px',
@@ -79,6 +139,7 @@ const TkNo = () => {
                 borderRadius: borderRadiusLG,
             }}
         >
+            {contextHolder}
             <div>
                 <Title level={3}>Tài Khoản Nợ</Title>
             </div>
